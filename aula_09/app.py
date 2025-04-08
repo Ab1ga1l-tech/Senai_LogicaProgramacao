b8036059d71de90393a24388326dbbc5d5a4fbd3
Ab1ga1l-tech <150353834+Ab1ga1l-tech@users.noreply.github.com>
2025-04-08 20:55:00 -0300

    Update app.py

--- aula_09/app.py
+++ aula_09/app.py
@@ -1,12 +1,12 @@
 # nove itens no cadastro 
 
-from parte1 import*
+from parte1 import* 
 
 def menu():
     
     while True:
         print("1 - Cadastrar aluno")
-        print("2 - Adicionar disciplina")
+        print("2- Listar Alunos ")
         print("3 - Adicionar nota")
         print("4 - Consultar boletim")
         print("5 - Consultar aluno pelo nome")
@@ -14,6 +14,7 @@ def menu():
         print("7 - Excluir aluno")
         print("8 - Excluir disciplina")
         print("9 - Sair")
+        print("2 - Adicionar disciplina")
 
         opcao = input("Escolha: ")
         if opcao == "1":
@@ -21,7 +22,14 @@ def menu():
         elif opcao == "2":
             listar_alunos()    
         elif opcao == "3":
-            break
+            Adicionar_nota()
+        elif opcao == "4":
+            print()
+        else:
+            print("Número inválido :( tente novamente")
+
+    # Verifica se o aluno existe
+    
         
 
 if __name__ == "__main__":
