6bf986e7f066e1ebfa1cd6ea8ab8347b7a00babc
Ab1ga1l-tech <150353834+Ab1ga1l-tech@users.noreply.github.com>
2025-04-09 20:42:14 -0300

    Update app.py

--- aula_09/app.py
+++ aula_09/app.py
@@ -5,7 +5,7 @@ def menu():
     while True:
         print("\n===== MENU =====")
         print("1 - Cadastrar aluno")
-        print("2 - Listar alunos")
+        print("2 - Listar todos os alunos")
         print("3 - Adicionar nota")
         print("4 - Consultar boletim")
         print("5 - Consultar aluno pelo nome")
@@ -23,8 +23,18 @@ def menu():
             Adicionar_nota()
         elif opcao == "4":
             Consultar_boletim()
+        elif  opcao =="5":
+            Consultar_aluno_nome()
+        elif opcao == "6":
+            Editar_dados()
+        elif opcao == "7":
+            Exclui_aluno()   
+        elif opcao == "8":
+            Exclui_disciplina()
+        elif opcao =="9":
+            break   
         else:
-            print("Número inválido :( tente novamente")
-            break
+            print("opção inválida")
     # Verifica se o aluno existe
-    
+if __name__=='__main__':
+    menu()
