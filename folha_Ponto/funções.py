94b4aabf26c488c27849b2569ef66292bef3bddf
Ab1ga1l-tech <150353834+Ab1ga1l-tech@users.noreply.github.com>
2025-04-22 20:50:27 -0300

    Update funções.py

--- folha_Ponto/funções.py
+++ folha_Ponto/funções.py
@@ -4,16 +4,52 @@ import re
 
 # Criação da tabela
 def criar_tabela():
-    con = sqlite3.connect("alunos.db")
+    con = sqlite3.connect("professores.db")
     cur = con.cursor()
     cur.execute("""
-        CREATE TABLE IF NOT EXISTS alunos (
+        CREATE TABLE IF NOT EXISTS professores (
             cpf TEXT PRIMARY KEY,
             nome TEXT NOT NULL,
-            data_nasc TEXT NOT NULL,
-            serie INTEGER NOT NULL,
-            email TEXT
+            data TEXT NOT NULL,
+            area text NOT NULL,
+                
         )
     """)
     con.commit()
     con.close()
+
+def validar_cpf(cpf):
+    return cpf.isdigit() and len(cpf) == 11
+
+# Validação simples de e-mail
+
+# Inserção de aluno
+def inserir_aluno(cpf, nome, data, area):
+    try:
+        con = sqlite3.connect("professores.db")
+        cur = con.cursor()
+        cur.execute("INSERT INTO professores (cpf, nome, data_nasc, serie, email) VALUES (?, ?, ?, ?, ?)",
+                    (cpf, nome, data_nasc, serie, email))
+        con.commit()
+        con.close()
+        return True
+    except sqlite3.IntegrityError:
+        return False
+
+# Consulta aluno por CPF
+def consultar_aluno(cpf):
+    con = sqlite3.connect("professores.db")
+    cur = con.cursor()
+    cur.execute("SELECT * FROM alunos WHERE cpf = ?", (cpf,))
+    aluno = cur.fetchone()
+    con.close()
+    return aluno
+
+# Listar todos os alunos
+def listar_todos():
+    con = sqlite3.connect("professores.db")
+    cur = con.cursor()
+    cur.execute("SELECT * FROM alunos")
+    lista = cur.fetchall()
+    con.close()
+    return lista
