8ce3f9950752cf4a90fbb761caacd45e7fe62a13
Ab1ga1l-tech <150353834+Ab1ga1l-tech@users.noreply.github.com>
2025-04-24 00:39:53 -0300

    Update app.py

--- folha_Ponto/app.py
+++ folha_Ponto/app.py
@@ -8,12 +8,13 @@ from cadastro import*
 def cadastrar():
     nome = nome_entry.get()
     data = data_entry.get()
-    hour = hour_entry.get()
+    hora = hour_entry.get()
     area = area_entry.get()
+    tipo = tipo_entry.get()
     jus = jus_entry.get()
 
 
-    if not  data or not nome or not area or not hour:
+    if not nome or not data or not hora or not area or not tipo:
         messagebox.showwarning("Você não preencheu todos os itens do cadastro")
         return
     
@@ -23,13 +24,13 @@ def cadastrar():
     except:
         messagebox.showerror("Erro", "Data deve estar no formato DD/MM/AAAA e Série deve ser número.")
         return
-    sucesso = inserir_aluno( nome, data)
+    sucesso = inserir_aluno(nome, data, hora, area, tipo, jus)
     if sucesso:
-        messagebox.showinfo("Sucesso", "Aluno cadastrado com sucesso!")
+        messagebox.showinfo("Sucesso", "Professor cadastrado com sucesso!")
         limpar_campos()
     else:
-        messagebox.showerror("Erro", "CPF já cadastrado!")
-    
+        messagebox.showerror("Erro", "Erro ao cadastrar professor.")
+
 #def consultar():
         
 
@@ -53,16 +54,18 @@ def limpar_campos():
 janela = tk.Tk()
 
 def listar():
-    alunos = listar_todos()
-    texto = tk.Text(janela, width=70, height=20)
-    texto.grid(row=7, column=0, columnspan=2, sticky="e", padx=5, pady=5)
-    for a in alunos:
-        texto.insert(tk.END, f"""CPF: {a[0]}, Nome: {a[1]}, Nasc: {a[2]}, Série: {a[3]}, Email: {a[4]}""")
+    professores = listar_todos()
+    texto = tk.Text(janela, width=90, height=20)
+    texto.grid(row=8, column=0, columnspan=3, sticky="e", padx=5, pady=5)
+    for p in professores: 
+        texto.insert(tk.END, f"Nome: {p[0]},Data: {p[1]}, Hora: {p[2]}, Área: {p[3]}, Tipo: {p[4]}, Justificativa: {p[5]}\n")
+
 
 
 #formulário
 tk.Label(janela, text="Nome*").grid(row=1, column=0, sticky="e", padx=5, pady=5)
-nome_entry = ttk.Combobox(janela, width=30)
+nome_entry = ttk.Combobox(janela, width=28)
+nome_entry['values'] = ["Lucia Helena Souza Santos", "Verônica Lopes do Santos","Sandra Maria Coelho de Oliveira","José Sérgio Pereira da Costa"]
 nome_entry.grid(row=1, column=1)
 
 tk.Label(janela, text="Data: (DD/MM/AAAA) *").grid(row=2, column=0, sticky="e", padx=5, pady=5)
@@ -75,10 +78,11 @@ hour_entry.grid(row=3, column=1)
 
 tk.Label(janela, text="Área *").grid(row=4, column=0, sticky="e", padx=5, pady=5)
 area_entry = ttk.Combobox(janela, width=28)
-area_entry['values'] = ["Matemática", "Português", "História", "Inglês", "Educação Física"]  # você pode alterar essa lista
+area_entry['values'] = ["Matemática", "Português", "História", "Inglês", "Educação Física","artes"," física"," química"," biologia ", "Sociologia", "filosofia", "geografia", "funcionários"
+"sasp", "multimeios", "laboratório de infomática"] 
 area_entry.grid(row=4, column=1)
 
-tk.Label(janela, text=" Tipo*").grid(row=5, column=0, sticky="e", padx=5, pady=5)
+tk.Label(janela, text=" Tipo (entrada/saída)*").grid(row=5, column=0, sticky="e", padx=5, pady=5)
 tipo_entry = ttk.Combobox(janela, width=28)
 tipo_entry['values'] = ["entrada", "Saída"]  
 tipo_entry.grid(row=5, column=1)
