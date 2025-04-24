88f024e2ad81b90c40284c7d3a5f8916bb6f3319
Ab1ga1l-tech <150353834+Ab1ga1l-tech@users.noreply.github.com>
2025-04-24 20:45:13 -0300

    Create login.py

---
+++ boletim/login.py
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="pagina.css">
+    <script src="pagina.css"> </script>
+    <link rel="stylesheet" href="cadastro.html">
+</head>
+<body>
+    
+    <div id="login">
+        <h1>login</h1>
+        <form id="formLogin">
+            <label>Usuario</label>
+            <input type="text">
+            <br>
+            <label>Senha</label>
+            <input type="text">
+            <button submit > Entrar</button>
+    </div>
+</body>
+</html>
