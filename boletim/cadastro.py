9b4f1cce32473214ccb98df381a57b821f9a03ea
Ab1ga1l-tech <150353834+Ab1ga1l-tech@users.noreply.github.com>
2025-04-24 20:45:29 -0300

    Create cadastro.py

---
+++ boletim/cadastro.py
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Minha Página Interativa</title>
+    <link rel="stylesheet" href="pagina.css">
+    <script src="pagina.js"> </script>
+</head>
+<body>
+       <div id="Cadastro">
+        <h1>Cadastro de Boletim</h1>
+       <form id="FormCadastro">
+        <label>Nome</label>
+       <input type="text">
+       <br>
+       <label>data nascimento</label>
+       <input type="text">
+       <br>
+       <label>série</label>
+       <input type="text">
+       <br>
+       <label>nota 1</label>
+       <input type="text">
+       <br>
+       <label>nota 2</label>
+       <input type="text">
+       <br>
+       <label>nota 3</label>
+       <input type="text">
+       </form>
+       <br>
+       <button bg="green">Salvar</button>
+       <button>Listar</button>
+       </div>
+
+</body>
+</html>
